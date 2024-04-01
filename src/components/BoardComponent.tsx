23f9b02f5b342f2076f6306cd2c7fbb7683fdcc6
ka9mal6tik <vladimyr.kilko@gmail.com>
2024-04-01 14:37:24 +0300

    add func reverse

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -3,6 +3,7 @@ import {Board} from "../models/Board";
 import {Cell} from "../models/Cell";
 import CellComponent from "./CellComponent";
 import {Player} from "../models/Player";
+import {Colors} from "../models/Colors";
 
 interface BoardProps {
     board: Board;
@@ -22,10 +23,7 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard, currentPlayer, swapPla
             if(cell.figure?.color === currentPlayer?.color){
                 setSelectedCell(cell);
             }
-
         }
-
-
     }
 
     useEffect(() => {
@@ -41,23 +39,46 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard, currentPlayer, swapPla
         const newBoard = board.getCopyBoard();
         setBoard(newBoard);
     }
+    if(currentPlayer?.color === Colors.WHITE) {
+        return (
+            <div className="board">
+                {board.cells.map((row, index) =>
 
-    return (
-        <div className="board">
-            {board.cells.map((row, index) =>
-                <React.Fragment key={index}>
-                    {row.map(cell =>
-                    <CellComponent
-                    click={click}
-                    cell={cell}
-                    key={cell.id}
-                    selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
-                    />
+                        <React.Fragment key={index}>
+                            {row.map(cell =>
+                                <CellComponent
+                                    click={click}
+                                    cell={cell}
+                                    key={cell.id}
+                                    selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                                />
+                            )}
+                        </React.Fragment>
                     )}
-                </React.Fragment>
-            )}
-        </div>
-    );
+            </div>
+        );
+    }
+    else {
+         return (
+            <div className="board">
+                {board.cells
+                    .slice().reverse()
+                    .map((row, index) =>
+
+                        <React.Fragment key={index}>
+                            {row.slice().reverse().map(cell =>
+                                <CellComponent
+                                    click={click}
+                                    cell={cell}
+                                    key={cell.id}
+                                    selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                                />
+                            )}
+                        </React.Fragment>
+                    )}
+            </div>
+        );
+    }
 };
 
 export default BoardComponent;
